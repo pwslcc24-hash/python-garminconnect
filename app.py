b41a3baf4ba1296d8c4d8649d5b05ba6d9bf8c10
pwslcc24-hash <pwslcc24@gmail.com>
2025-12-20 22:12:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from garminconnect import Garmin
